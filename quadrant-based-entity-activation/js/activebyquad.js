ebf4570407e00a295f486e25dd6077f54e659705
Christian Gutierrez <gutierrezchristianf@gmail.com>
2021-03-01 21:01:45 -0800

    new objects are correctly activating/deactivating

--- quadrant-based-entity-activation/js/activebyquad.js
+++ quadrant-based-entity-activation/js/activebyquad.js
@@ -1,24 +1,51 @@
 //Query user and text elements
 const cameraEl = document.querySelector("[networked-avatar]"); //query the user
-const posTextEl = document.querySelector("#pos-text");
-const angleTextEl = document.querySelector("#angle-text");
-const quadTextEl = document.querySelector("#quad-text");
-const sceneEl = document.querySelector('a-scene');
-let interactablesAmountBefore = document.querySelector(".interactable").length;
+//const sceneEl = document.querySelector('a-scene');
+//let interactablesBefore = document.querySelectorAll(".interactable").length;
+let interactablesBefore = document.querySelectorAll("[gltf-model-plus][networked], [media-video][networked], [media-image][networked], [media-pdf][networked]").length;
+
+// //Text entities
+// const posTextEl = document.querySelector("#pos-text");
+// const angleTextEl = document.querySelector("#angle-text");
+// const quadTextEl = document.querySelector("#quad-text");
 
 //Query all entity assets
 let allEntityArray = document.querySelectorAll("[gltf-model-plus][networked], [media-video][networked], [media-image][networked], [media-pdf][networked]");
-allEntityArray.forEach(element => element.setAttribute("visible", false));
 
 const ORIGIN_X = 0;
 const ORIGIN_Z = 0;
 
-let currQuad = null;
-let prevQuad = null;
+let userCurrQuad = null;
+let userPrevQuad = null;
 
+determineUserQuad();
 assignQuadsToEntities(allEntityArray);
 
+//Continously update scene info
+setInterval(function() {
+  //Get update on user position and current quadrant
+  determineUserQuad();
+
+  //let interactablesAfter = document.querySelectorAll(".interactable").length;
+  let interactablesAfter = document.querySelectorAll("[gltf-model-plus][networked], [media-video][networked], [media-image][networked], [media-pdf][networked]").length;
+  if (interactablesAfter != interactablesBefore){
+    interactablesBefore = interactablesAfter;
+    allEntityArray = document.querySelectorAll("[gltf-model-plus][networked], [media-video][networked], [media-image][networked], [media-pdf][networked]");
+    assignQuadsToEntities(allEntityArray);
+  }
+    
+  //updateHUDText();
+  
+  //Check if user left the quadrant
+  if(userPrevQuad != userCurrQuad){
+    //Update visible assets
+    updateVisible(userCurrQuad, userPrevQuad); 
+    userPrevQuad = userCurrQuad;
+  }   
+}, 1000/80);
+
 function assignQuadsToEntities(entityArr){
+  //entityArr.forEach(element => element.setAttribute("visible", false));
 
   //Calculate and assign corresponding "quad" class to each entity 
   for(let i = 0; i < entityArr.length; i++){
@@ -29,61 +56,58 @@ function assignQuadsToEntities(entityArr){
 
     if( entityQuad === 1 ){
       currEntity.classList.add("quad1");
+      if(userCurrQuad != 1){
+        currEntity.setAttribute("visible", false)
+      }
     }
     else if( entityQuad === 2){
       currEntity.classList.add("quad2");
+      if(userCurrQuad != 2){
+        currEntity.setAttribute("visible", false)
+      }
     }
     else if( entityQuad === 3){
       currEntity.classList.add("quad3");
+      if(userCurrQuad != 3){
+        currEntity.setAttribute("visible", false)
+      }
     }
     else if( entityQuad === 4){
       currEntity.classList.add("quad4");
+      if(userCurrQuad != 4){
+        currEntity.setAttribute("visible", false)
+      }
     }
     else{
       console.log("Could not determine quadrant");
     }
+
   }
 }
 
-//Continously update scene info
-setInterval(function() {
-  // let interactClassAfter = document.querySelector(".interactable").length;
-  // if (interactablesAmountAfter != interactablesAmountBefore){
-  //   interactablesAmountBefore = interactClassAfter;
-  // }
-
-  let localPos = cameraEl.object3D.position;
-  let worldPos = cameraEl.object3D.getWorldPosition();
-  
-  let playerX = worldPos.x;
-  let playerZ = worldPos.z
+function determineUserQuad(){
+  //let localPos = cameraEl.object3D.position;
+  let playerPos = cameraEl.object3D.getWorldPosition();
  
   //Calculate user current angle and quadrant
-  let currAngle = calcAngle(ORIGIN_X, ORIGIN_Z, playerX, playerZ).toFixed(2);
-  currQuad = calcQuad(currAngle);
-  
+  let currAngle = calcAngle(ORIGIN_X, ORIGIN_Z, playerPos.x, playerPos.z).toFixed(2);
+  userCurrQuad = calcQuad(currAngle);
+}
+
+function updateHUDText(){
   //Update user position text
-  posTextEl.setAttribute("value", "Position: " + worldPos.x.toFixed(2) + " " + worldPos.y.toFixed(2) + " " + worldPos.z.toFixed(2) + " ");
- 
+  posTextEl.setAttribute("value", "Position: " + playerPos.x.toFixed(2) + " " + playerPos.y.toFixed(2) + " " + playerPos.z.toFixed(2) + " "); 
   //Update user angle text
-  angleTextEl.setAttribute("value", "\n\nAngle: " + currAngle);
-  
+  angleTextEl.setAttribute("value", "\n\nAngle: " + currAngle); 
   //Update user quadrant text
-  quadTextEl.setAttribute("value", "\n\n\n\nQuadrant: " + currQuad);
-  
-  //Check if user left the quadrant
-  if(prevQuad != currQuad){
-    //Update visible assets
-    updateVisible(currQuad, prevQuad); 
-    prevQuad = currQuad;
-  }   
-}, 1000/80);
+  quadTextEl.setAttribute("value", "\n\n\n\nQuadrant: " + userCurrQuad);
+}
 
 //Calculate user's angle respective to origin
 function calcAngle(x1, z1, x2, z2) {
     var result = Math.atan2(z1 - z2, x2 - x1) * (180 / Math.PI);
     return result > 0 ? result : 360 + result;
-};
+}
 
 //Calculate the visited quadrant
 function calcQuad(userAngle){
