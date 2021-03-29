0184b05054ce71b11ee33d4c39ba9bec911d6553
Christian Gutierrez <gutierrezchristianf@gmail.com>
2021-03-29 14:46:01 -0700

    fix null check for bg sounds

--- js/active-by-location.js
+++ js/active-by-location.js
@@ -193,10 +193,10 @@ function updateActiveMusic(strataCurr, strataPrev){
   console.log("strataCurr: " + strataCurr);
   console.log("strataPrev: " + strataPrev);
   
-  if(strataCurr){
+  if(strataCurr != null){
     strataMusicEntities[strataPrev].components.sound.stopSound();
   }
-  if(strataPrev){
+  if(strataPrev != null){
     strataMusicEntities[strataCurr].components.sound.playSound();
   }
 }
